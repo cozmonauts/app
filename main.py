aa43c43e9d5d81eff2debdf8a0d85aa4bffab20a
Tyler Filla <tylerfilla@hotmail.com>
2019-05-04 19:07:57 -0500

    Make Cozmo say some stuff

--- main.py
+++ main.py
@@ -3,5 +3,13 @@
 # Copyright 2019 The Cozmonaut Contributors
 #
 
+import cozmo
+
+
+async def my_program(robot):
+    await robot.say_text('my rahfulcopter goes soysoysoysoysoysoy').wait_for_completed()
+
+
 if __name__ == '__main__':
-    print('Hello, world!')
+    cozmo.robot.Robot.drive_off_charger_on_connect = False
+    cozmo.run_program(my_program, use_viewer=True)
