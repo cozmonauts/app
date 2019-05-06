444de7424d8beec13153611d4d60019eb00154b5
Tyler Filla <tylerfilla@hotmail.com>
2019-05-06 02:11:52 -0500

    Get Cozmo driving from Python from C

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
 
 #include "service/python.h"
 #include "global.h"
@@ -13,11 +14,16 @@ int main(int argc, char* argv[]) {
   g_mut->argc = argc;
   g_mut->argv = (const char**) argv;
 
+  // Spin up Python service
   service_load(SERVICE_PYTHON);
   service_start(SERVICE_PYTHON);
 
-  // TODO
+  // Call interact operation
+  service_call(SERVICE_PYTHON, service_python_fn_op_exec, (const void*) service_python_op_interact, NULL);
 
+  sleep(25);
+
+  // Shut down Python service
   service_stop(SERVICE_PYTHON);
   service_unload(SERVICE_PYTHON);
 }
