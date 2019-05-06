035039b2f8e9a449ebddd548d6bc19ddedb00624
Tyler Filla <tylerfilla@hotmail.com>
2019-05-06 01:28:06 -0500

    Add linenoise dependency

--- src/main.c
+++ src/main.c
@@ -4,18 +4,16 @@
  */
 
 #include <stdio.h>
-#include <Python.h>
+#include <stdlib.h>
+
+#include <linenoise.h>
 
 int main() {
-  printf("Hello, world! (from C)\n");
-
-  // Initialize the Python VM
-  Py_Initialize();
-
-  PyRun_SimpleString("print(\'Hello, world! (from Python from C)\')");
-
-  if (Py_FinalizeEx() < 0) {
-    fprintf(stderr, "failed to finalize Python VM\n");
-    return 1;
+  char* line;
+  while ((line = linenoise("C:\\> ")) != NULL) {
+    if (line[0] != '\0') {
+      printf("echo: %s\n", line);
+    }
+    free(line);
   }
 }
