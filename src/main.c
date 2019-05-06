f6f4f5e48c7e09b8c1f1cf2277a98e3e21ddb148
Tyler Filla <tylerfilla@hotmail.com>
2019-05-06 01:15:01 -0500

    Hello from Python from C!

--- src/main.c
+++ src/main.c
@@ -4,7 +4,18 @@
  */
 
 #include <stdio.h>
+#include <Python.h>
 
 int main() {
-  printf("Hello, world!\n");
+  printf("Hello, world! (from C)\n");
+
+  // Initialize the Python VM
+  Py_Initialize();
+
+  PyRun_SimpleString("print(\'Hello, world! (from Python from C)\')");
+
+  if (Py_FinalizeEx() < 0) {
+    fprintf(stderr, "failed to finalize Python VM\n");
+    return 1;
+  }
 }
