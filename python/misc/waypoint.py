bd44fa71ed1c942ae536ca70f17c4c276441f75f
Ericonway75 <bxbmilkbxb@yahoo.com>
2019-05-06 12:56:43 -0500

    Add files via upload

--- python/misc/waypoint.py
+++ python/misc/waypoint.py
@@ -1,12 +1,10 @@
 import cozmo
-import chargerReturn
 from cozmo.util import distance_mm, speed_mmps
 
 def driveFWD(robot: cozmo.robot.Robot):
-    robot.drive_straight(distance_mm(250), speed_mmps(50)).wait_for_completed()
-   # waypoint = robot.pose
-    #robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
-    #robot.go_to_pose(waypoint).wait_for_completed()
-    chargerReturn.cozmo_program(robot)
+    #robot.drive_straight((distance_mm(150), speed_mmps(50)).wait_for_completed(None))
+    waypoint = robot.pose
+    robot.drive_straight((distance_mm(150), speed_mmps(50)).wait_for_completed(None))
+    robot.go_to_pose(waypoint).wait_for_completed()
 
 cozmo.run_program(driveFWD)
