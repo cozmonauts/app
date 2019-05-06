74ba4285d6a6f1a03b9394155e1acbf78ddd4eda
Ericonway75 <bxbmilkbxb@yahoo.com>
2019-05-06 14:05:34 -0500

    Add files via upload

--- python/waypoint.py
+++ python/waypoint.py
@@ -1,10 +1,13 @@
 import cozmo
 from cozmo.util import distance_mm, speed_mmps
 
-def driveFWD(robot: cozmo.robot.Robot):
+
+def drivefwd(robot: cozmo.robot.Robot):
+
     #robot.drive_straight((distance_mm(150), speed_mmps(50)).wait_for_completed(None))
     waypoint = robot.pose
     robot.drive_straight((distance_mm(150), speed_mmps(50)).wait_for_completed(None))
     robot.go_to_pose(waypoint).wait_for_completed()
 
-cozmo.run_program(driveFWD)
+
+cozmo.run_program(drivefwd)
