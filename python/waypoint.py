059183010c9b9314ef087891ab39f3b11f3bf4c1
herm95 <49575636+herm95@users.noreply.github.com>
2019-05-08 13:07:33 -0500

    Update waypoint.py

--- python/waypoint.py
+++ python/waypoint.py
@@ -6,7 +6,7 @@ def drivefwd(robot: cozmo.robot.Robot):
 
     #robot.drive_straight((distance_mm(150), speed_mmps(50)).wait_for_completed(None))
     waypoint = robot.pose
-    robot.drive_straight((distance_mm(150), speed_mmps(50)).wait_for_completed(None))
+    robot.drive_straight((distance_mm(150), speed_mmps(50)).wait_for_completed())
     robot.go_to_pose(waypoint).wait_for_completed()
 
 
